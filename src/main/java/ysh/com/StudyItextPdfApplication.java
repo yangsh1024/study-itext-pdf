42dbf85bc8989e1c1bd8d0eee0022a0b092e66e3
yangsh <shouhao.yang@jdongtech.com>
2021-05-06 16:52:26 +0800

    init

---
+++ src/main/java/ysh/com/StudyItextPdfApplication.java
@@ -0,0 +1,13 @@
+package ysh.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyItextPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyItextPdfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ysh/com/StudyItextPdfApplicationTests.java
@@ -0,0 +1,12 @@
+package ysh.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyItextPdfApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
